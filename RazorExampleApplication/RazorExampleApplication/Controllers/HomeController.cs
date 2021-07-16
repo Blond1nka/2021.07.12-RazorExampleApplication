eaa962e87c9e8f9d07265250f1b67d5ac2008a41
Blond1nka <po.heize@gmail.com>
2021-07-16 20:33:10 +0300

    Initial commit

---
+++ RazorExampleApplication/RazorExampleApplication/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RazorExampleApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorExampleApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            IndexModel model = new IndexModel()
+            {
+                Name = "Jonas",
+                Profession = "Devoloper"
+            };
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            IndexModel model = new IndexModel()
+            {
+                Name = "fill me"
+            };
+            return View(model);
+        }
+
+        public IActionResult DisplayRegister(IndexModel model)
+        {
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RazorExampleApplication/RazorExampleApplication/Models/IndexModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorExampleApplication.Models
+{
+    public class IndexModel
+    {
+        public string Name { get; set; }
+
+        public string Profession { get; set; }
+    }
+}
